bde17266bb45ab219dfae50ab5e9b3e851330c44
krutkowski86 <krzysztof.rutkowski.86@gmail.com>
2017-12-28 13:11:54 +0100

    user route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,8 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './core/components/login/login.component';
-import { UserComponent } from './user/user.component';
+import { UserComponent } from './core/components/user/user.component';
+import { PageNotFoundComponent } from './core/components/page-not-found/page-not-found.component';
 
 const routes: Routes = [
   {
@@ -22,7 +23,8 @@ const routes: Routes = [
         loadChildren: 'app/user/dashboard/dashboard.module#DashboardModule'
       }
     ]
-  }
+  },
+  { path: '**', component: PageNotFoundComponent }
 ];
 
 @NgModule({
--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { SharedModule } from '../shared/shared.module';
 import { LoginComponent } from './components/login/login.component';
+import { UserComponent } from './components/user/user.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 
 @NgModule({
   imports: [SharedModule.forRoot()],
-  declarations: [LoginComponent]
+  declarations: [LoginComponent, UserComponent, PageNotFoundComponent]
 })
 export class CoreModule {}
